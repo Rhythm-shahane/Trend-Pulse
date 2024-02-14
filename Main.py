ed54c6e196a8fe4afc4e262ac9ea9165c05ab55c
Devansh Shahane <128963083+Rhythm-shahane@users.noreply.github.com>
2024-02-15 03:17:38 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,33 +6,33 @@ st.set_page_config(
 )
 streamlit run Main.py
 st.markdown(
-    """# 📈 **Stockastic**
+    """# 📈 **Trend Pulse**
 ### **Predicting Stocks with ML**
 
-**Stockastic is an ML-powered stock price prediction app built with Python and Streamlit. It utilizes machine learning models to forecast stock prices and help investors make data-driven decisions.**
+**Trend Pulse is an ML-powered stock price prediction app built with Python and Streamlit. It utilizes machine learning models to forecast stock prices and help investors make data-driven decisions.**
 
 ## 🏗️ **How It's Built**
 
-Stockastic is built with these core frameworks and modules:
+Trend Pulse is built with these core frameworks and modules:
 
 - **Streamlit** - To create the web app UI and interactivity 
-- **YFinance** - To fetch financial data from Yahoo Finance API
-- **StatsModels** - To build the ARIMA time series forecasting model
+- **LSTM** - To build the Long Short Term Memory model
 - **Plotly** - To create interactive financial charts
+- **Keras** - To Train the Model layer by layer.
+- **Tensorflow** - To Primarily Build & Train LSTM Neural Network model.
 
 The app workflow is:
 
-1. User selects a stock ticker
-2. Historical data is fetched with YFinance
-3. ARIMA model is trained on the data 
+1. User feeds the CSV file.
+2. Historical data is fetched with CSV file.
+3. LSTM model is trained on the data 
 4. Model makes multi-day price forecasts
 5. Results are plotted with Plotly
 
 ## 🎯 **Key Features**
 
-- **Real-time data** - Fetch latest prices and fundamentals 
 - **Financial charts** - Interactive historical and forecast charts
-- **ARIMA forecasting** - Make statistically robust predictions
+- **LSTM forecasting** - Make statistically robust predictions
 - **Backtesting** - Evaluate model performance
 - **Responsive design** - Works on all devices
 
@@ -43,7 +43,7 @@ The app workflow is:
 1. Clone the repo
 
 ```bash
-git clone https://github.com/user/stockastic.git
+git clone https://github.com/Rhythm-shahane/Trend-Pulse.git
 ```
 
 2. Install requirements
@@ -54,28 +54,25 @@ pip install -r requirements.txt
 
 3. Change directory
 ```bash
-cd streamlit_app
+cd app.py
 ```
 
 4. Run the app
 
 ```bash
-streamlit run 00_😎_Main.py
+streamlit run app.py
 ```
-
-The app will be live at ```http://localhost:8501```
 
 ## 📈 **Future Roadmap**
 
 Some potential features for future releases:
 
-- **More advanced forecasting models like LSTM**
+- **More advanced forecasting models like Transformer**
 - **Quantitative trading strategies**
 - **Portfolio optimization and tracking**
 - **Additional fundamental data**
 - **User account system**
+- **Real Time Data Fetching**
 
 ## **⚖️ Disclaimer**
 **This is not financial advice! Use forecast data to inform your own investment research. No guarantee of trading performance.**
-"""
-)
